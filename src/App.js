bdf5a4a69b66893e67aa14bb891096fe907ed1a2
Leonel Sosa <leo.vm.cba@gmail.com>
2022-09-21 20:10:39 -0300

    first commit. Add header component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <Fragment>
+      <Header
+        titulo="Cotizador de Prestamos"
+        descripcion="Llena el formulario y obten una cotización"
+      />
+    </Fragment>
+    );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+function Header({titulo, descripcion}) {
+    console.log(titulo, descripcion)
+    return (
+      <>
+        <h1 className="text-5xl text-center text-blue-200">{titulo}</h1>
+        <p className="text-lg text-white text-center mt-2 font-semibold">{descripcion}</p>
+      </>
+    );
+  }
+
+export default Header;
